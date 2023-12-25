d0b716d16345ceb4431c7ce0652ed28e9828bf2c
Your Name <you@example.com>
2023-12-26 00:18:18 +0530

    deleted:    .env 	modified:   .gitignore 	deleted:    server/app.js 	modified:   src/components/Messages/ChatInput.jsx

--- src/components/Messages/ChatInput.jsx
+++ src/components/Messages/ChatInput.jsx
@@ -45,16 +45,19 @@ function ChatInput() {
     const getResponse = async () => {
         setIsResponseLoading(true);
         try {
-            const response = await fetch("http://localhost:8000/chat", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    history,
-                    prompt: userInput,
-                }),
-            });
+            const response = await fetch(
+                "https://chatbot-api-pfz2.onrender.com/chat",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        history,
+                        prompt: userInput,
+                    }),
+                }
+            );
             const data = await response.json();
             setMessages((msg) => [...msg, { role: "model", parts: data }]);
             await addDoc(collectionRef, {
