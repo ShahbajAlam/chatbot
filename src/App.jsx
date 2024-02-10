9f677fa61a740cc6f966e52d5782f33494e45792
Your Name <you@example.com>
2024-02-11 01:30:31 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     const { userID } = useAuth();
 
     return (
-        <div className="w-[100vw] h-[100dvh] flex justify-center items-center bg-gradient-to-t from-[#093028] to-[#3c8f7e] overflow-hidden md:w-[85vw] md:rounded-3xl md:h-[90vh] lg:w-[70vw] lg:h-[100vh]">
+        <div className="w-[100vw] h-[100svh] flex justify-center items-center bg-gradient-to-t from-[#093028] to-[#3c8f7e] overflow-hidden md:w-[85vw] md:rounded-3xl md:h-[90vh] lg:w-[70vw] lg:h-[100vh]">
             <Toaster />
             {!userID && <Login />}
             {userID && <Chat />}
